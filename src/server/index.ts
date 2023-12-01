69e4b1f8e4cca639b30d5affd8f42d7bed5e3375
njrp94 <njrp.1994@gmail.com>
2023-12-01 09:18:31 -0300

    jwt fix

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,7 +2,6 @@ import express from "express";
 import bodyParser from "body-parser";
 import searchRoutes from "./routes/search.routes";
 import userRoutes from "./routes/user.routes"
-//import generateToken from '../middlewares/token.middleware';
 import cors from 'cors';
 import * as jwt from 'jsonwebtoken'; 
 import { v4 as uuidv4 } from 'uuid';
@@ -26,7 +25,6 @@ app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-//app.use(generateToken);
 app.use('/api/v1', searchRoutes);
 app.use('/api/v1', userRoutes);
 
